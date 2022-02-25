3704fd43f4c76c786520377aec042e8026bc72f0
Max <mmaxvojtkov@gmial.com>
2022-02-25 14:53:20 +0200

    added html & css

--- app/main/models.py
+++ app/main/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
+
+class help(models.Model):
+    help_type = models.CharField(max_length=20)
+    nume = models.CharField(max_length=255)
+    tel = models.CharField(max_length=15)
+    details = models.TextField(max_length=1000)
+    # pub_date = model.
 
 # Create your models here.
