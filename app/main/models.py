634b78a81809076974d71fabb928d599e613e62c
Max <mmaxvojtkov@gmial.com>
2022-02-25 15:01:10 +0200

    added sql schema

--- app/main/models.py
+++ app/main/models.py
@@ -1,11 +1,22 @@
 from pyexpat import model
 from django.db import models
 
-class help(models.Model):
+class Help(models.Model):
     help_type = models.CharField(max_length=20)
-    nume = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     tel = models.CharField(max_length=15)
     details = models.TextField(max_length=1000)
-    # pub_date = model.
+    pub_date = models.TimeField(auto_now_add=True)
+    mod_date = models.TimeField(auto_now=True)
+    link = models.URLField(max_length=1000, blank=True)
+
+class NeedHelp(models.Model):
+    help_type = models.CharField(max_length=20)
+    name = models.CharField(max_length=255)
+    tel = models.CharField(max_length=15)
+    details = models.TextField(max_length=1000)
+    pub_date = models.TimeField(auto_now_add=True)
+    mod_date = models.TimeField(auto_now=True)
+    link = models.URLField(max_length=1000, blank=True)
 
 # Create your models here.
