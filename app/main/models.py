c5075b9536c3ab4705c94c0cd4634616da92f35c
Max <max.vojtkov@gmail.com>
2022-02-27 00:15:32 +0100

    added datetime field

--- app/main/models.py
+++ app/main/models.py
@@ -6,10 +6,10 @@ class Help(models.Model):
     name = models.CharField(max_length=255)
     tel = models.CharField(max_length=15)
     details = models.TextField(max_length=1000)
-    pub_date = models.TimeField(auto_now_add=True)
-    mod_date = models.TimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mod_date = models.DateTimeField(auto_now=True)
     link = models.URLField(max_length=1000, blank=True)
-    
+
     class Adress(models.TextChoices):
         chisinau = "chisinau", "chisinau"
         balti = "balti", "balti"
@@ -59,8 +59,8 @@ class NeedHelp(models.Model):
     name = models.CharField(max_length=255)
     tel = models.CharField(max_length=15)
     details = models.TextField(max_length=1000)
-    pub_date = models.TimeField(auto_now_add=True)
-    mod_date = models.TimeField(auto_now=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    mod_date = models.DateTimeField(auto_now=True)
     link = models.URLField(max_length=1000, blank=True)
     # address = models.CharField(max_length=1000, default="Moldova")
     valabil = models.BooleanField(default=True)
