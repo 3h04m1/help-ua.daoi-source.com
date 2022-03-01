9595dc26e4195a7136c2e1113e417af3c6b912ab
Max <max.vojtkov@gmail.com>
2022-03-01 04:08:01 +0300

    seo optimise

--- app/core/urls.py
+++ app/core/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
 
+from .sitemaps import StaticSitemap
 
+sitemaps = {
+    # 'help': HelpSitemap,
+    # 'need_help':NeedHelpSitemap,
+    'static':StaticSitemap
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls"))
+    path('', include("main.urls")),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap')
 ]
